cc94b21f6ba2264e7f05f9475a89edbc101a2304
A601-00 <tnk.dat.class@gmail.com>
2022-05-12 15:44:20 +0900

    ボタンを消す

--- v0512/Form1.Designer.cs
+++ v0512/Form1.Designer.cs
@@ -29,25 +29,28 @@ namespace v0512
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.mybutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // mybutton
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.mybutton.BackColor = System.Drawing.Color.Orange;
+            this.mybutton.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mybutton.ForeColor = System.Drawing.Color.Red;
+            this.mybutton.Location = new System.Drawing.Point(54, 100);
+            this.mybutton.Name = "mybutton";
+            this.mybutton.Size = new System.Drawing.Size(196, 93);
+            this.mybutton.TabIndex = 0;
+            this.mybutton.Text = "button1";
+            this.mybutton.UseVisualStyleBackColor = false;
+            this.mybutton.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mybutton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,7 +59,7 @@ namespace v0512
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button mybutton;
     }
 }
 
